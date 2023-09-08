a9e1bff22f38f715bc3cbf757e7804f39e13ba64
Kahiro-M <0204.kahiro@gmail.com>
2023-09-08 15:56:58 +0900

    ファイル単体でのコピー処理追加（ディレクトリが存在しない場合は作成）

--- copy_mod.py
+++ copy_mod.py
@@ -28,5 +28,33 @@ def copy_files(file_list, destination_dir):
     except Exception as e:
         print(f"エラーが発生しました: {e}")
 
+def copy_file(file_path, destination_dir, out_file_name):
+    try:
+        # コピー先ディレクトリが存在しない場合は作成する
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+
+        # コピー元ファイルが存在するか確認する
+        if not os.path.isfile(file_path):
+            print(f"Error: ファイル '{file_path}' が存在しません。スキップします。")
+            return None
+        else:
+            # ファイルをコピーする準備
+            file_name = out_file_name
+            destination_path = os.path.join(destination_dir, file_name)
+
+            # コピー先ディレクトリに同じファイルが存在するか確認する
+            if os.path.exists(destination_path):
+                print(f"ファイル '{destination_path}' はすでに存在します。スキップします。")
+                return None
+            else:
+                # ファイルをコピーする（更新日時などの情報を保持したまま）
+                shutil.copy2(file_path, destination_path)
+                print(f"ファイル '{file_path}' を '{destination_path}' にコピーしました。")
+                return destination_path
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+        return None
+
 if __name__ == '__main__':
     main()
